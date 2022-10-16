4e795109061a250374721564a3d55f82e6f089f3
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-16 19:00:40 -0400

    [FEAT] Show images and high res link

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,7 +5,21 @@ const Image = ({ img }) => {
     return ( 
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
-                <img src={largeImageURL} alt={tags} className="card-img-top" />
+                <img src={previewURL} alt={tags} className="card-img-top" />
+
+                <div className="card-body">
+                    <p className="card-text">{likes} Likes</p>
+                    <p className="card-text">{views} Views</p>
+                </div>
+
+                <div className="card-footer">
+                    <a
+                        href={largeImageURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-primary btn-block"
+                    >View Image</a>
+                </div>
             </div>
         </div>
     );
