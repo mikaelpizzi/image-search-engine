779ff07364bff05d0cf824a92a074a6f3bbd59c4
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-16 17:32:25 -0400

    [ADD] Validate form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import Error from "./Error";
+
 const Form = () => {
+
+    const [ term, saveTerm ] = useState('');
+    const [ error, saveError ] = useState(false);
+
+    const searchImages = e => {
+        e.preventDefault();
+
+        // Validate
+        if (term.trim() === '') {
+            saveError(true);
+            return;
+        }
+        saveError(false);
+
+        // Send search term to principal component
+    }
+
     return (  
-        <form>
+        <form
+            onSubmit={searchImages}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Search for an image, for example: Winter or Science"
+                        onChange={ e => saveTerm(e.target.value) }
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -17,6 +40,7 @@ const Form = () => {
                     />
                 </div>
             </div>
+            { error ? <Error message='Add a search term' /> : null }
         </form>
     );
 }
